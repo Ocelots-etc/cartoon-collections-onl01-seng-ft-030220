25e54e9db4e00b8957addf07f1c699c20ab586a6
Cat Gomez <catherinedgomez@gmail.com>
2020-03-12 16:44:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,39 +8,6 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map(&:capitalize).collect{ |element| element + "!" }
 end
-#  if array.select { |element| element.length > 5
-#element = 0
-#if array.each.count { |element| element.length > 4 }
-  #planeteer_calls.collect {|element| element.upcase + "!"}.split
-#{i = 0
-#if i > array.length == 5
-#end} = this line of code is NilClasss
-
-  #return elements
-  #elements = planeteer_calls.join(" ")
-#i = 0
-#while i < elements.length
-#  element = element[i]
-#  element[0] = element[0].upcase
-#  i += 1
-#end
-  #i = 0
-  #while i < 4
-    #if planeteer_calls.each { |element| element.count > 4 }
-  #  i += 1
-  #end
-  #else
- #planeteer_calls
-
-   #summon_captain_planet(planeteer_calls)
-#  summon_captain_planet(planeteers_calls) do |element|
-  #  element.join(" ").capitalize
-  #planeteers_calls.join(" ").capitalize.split("!")
-  #planeteers_collection = []
-  #i = 0
-  #while i < planeteer_calls.length
-
-
 
 
 def long_planeteer_calls(summon_captain_planet)
