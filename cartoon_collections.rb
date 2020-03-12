e5b4f74bbcf0f6dccd2a32728e4ccb086576e244
Cat Gomez <catherinedgomez@gmail.com>
2020-03-12 16:36:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,64 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index|
+   puts "#{index + 1}.#{name}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map(&:capitalize).collect{ |element| element + "!" }
 end
+#  if array.select { |element| element.length > 5
+#element = 0
+#if array.each.count { |element| element.length > 4 }
+  #planeteer_calls.collect {|element| element.upcase + "!"}.split
+#{i = 0
+#if i > array.length == 5
+#end} = this line of code is NilClasss
+
+  #return elements
+  #elements = planeteer_calls.join(" ")
+#i = 0
+#while i < elements.length
+#  element = element[i]
+#  element[0] = element[0].upcase
+#  i += 1
+#end
+  #i = 0
+  #while i < 4
+    #if planeteer_calls.each { |element| element.count > 4 }
+  #  i += 1
+  #end
+  #else
+ #planeteer_calls
+
+   #summon_captain_planet(planeteer_calls)
+#  summon_captain_planet(planeteers_calls) do |element|
+  #  element.join(" ").capitalize
+  #planeteers_calls.join(" ").capitalize.split("!")
+  #planeteers_collection = []
+  #i = 0
+  #while i < planeteer_calls.length
+
+
+
+
+def long_planeteer_calls(summon_captain_planet)
+  if summon_captain_planet.each ( |element| element.count > 5)
+
+  else
+  end
+
+end
+#if planeteer_calls.each { |element| element.count > 5 }
+#    i += 1
+  #if array[i]  {i.length >= 4 ? true : false }
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+#planeteer_calls
+    #yield(planeteer_calls[i]) |element|
+        #puts "#{element}"
