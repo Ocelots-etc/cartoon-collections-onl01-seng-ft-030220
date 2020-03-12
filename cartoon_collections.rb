a52fbaeaba80ebb6325ca2a4bf9f8ce2521d5f77
Cat Gomez <catherinedgomez@gmail.com>
2020-03-12 16:43:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -48,7 +48,7 @@ def long_planeteer_calls(summon_captain_planet)
   i = 0
   if summon_captain_planet.each.count < 5
     i += 1
-
+   planeteer_calls.map(&:capitalize).collect{ |element| element + "!" }
   else
       long_planeteer_calls = false
   end
